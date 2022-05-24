76faccd40fc209f7d1f5cbb6e0dcb375ba5cb2ce
Евгения Пискарева <evgen-zykova@mail.ru>
2022-05-25 00:08:54 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public static void main(String[] args) {
+        int amountTicket = 2_000;
+        int amountOneMile = 20;
+        int bonysMiles = amountTicket / amountOneMile;
+
+        System.out.println(bonysMiles);
+
+    }
+}
